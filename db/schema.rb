91710fcaefce64cf1c1dfecf87a0f517564cdb00
thinksource <foretribe@gmail.com>
2017-01-23 22:35:20 +1100

    init2

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,15 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140707111715) do
+ActiveRecord::Schema.define(version: 20140627005245) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "widgets", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.text     "description"
-    t.integer  "stock"
+    t.string   "provider"
+    t.string  "business"
+    t.string   "uid"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
